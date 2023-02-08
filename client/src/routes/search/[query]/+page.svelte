378ab8fed2afcb0136a15ebd5b8e5ef5412e1893
Yasir Seyrek <yasirsey@gmail.com>
2023-02-08 15:40:59 +0300

    bug fix

--- client/src/routes/search/[query]/+page.svelte
+++ client/src/routes/search/[query]/+page.svelte
@@ -2,7 +2,7 @@
     import { page } from "$app/stores";
     import { onMount } from "svelte";
   
-    const query = $page.url.pathname.split("/")[2].replace('%20', '');
+    const query = $page.url.pathname.split("/")[2];
     let missings  = []
   
     onMount(async () => {
